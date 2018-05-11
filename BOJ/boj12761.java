94023d5383465eb07ebc9456200541851afeeddf
KwonOyun <oyoon33@gmail.com>
2018-05-11 20:43:08 +0900

    Add files via upload

---
+++ BOJ/boj12761.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class boj12761 {
+
+	public static int A, B, N, M;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		BFS();
+		
+	}
+	public static void BFS() {
+		int dongkyu = A;
+		Queue<Integer> all = new LinkedList<Integer>();
+		
+	}
+
+}
