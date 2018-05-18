dcf450ae510313834163fa4be318151ba92257e5
Oyun <oyoon33@gmail.com>
2018-05-18 23:37:50 +0900

    Add files via upload

--- BOJ/boj2667.java
+++ BOJ/boj2667.java
@@ -10,7 +10,6 @@ public class boj2667 {
 	public static int count;
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		matrix = new int[N+1][N+1];
@@ -28,14 +27,12 @@ public class boj2667 {
 			for(int j=1; j<=N; j++) {
 				if(matrix[i][j] == 1 && visited[i][j] == false) {
 					DFS(i,j);
-					result[resultcount] = count;
+					result[resultcount++] = count;
 					count=0;
-					resultcount++;
 				}
 			}
 		}
-	
-		for(int i=0; i<resultcount; i++) {
+		for(int i=0; i<resultcount-1; i++) {  //오름차순 정렬
 			for(int j=1; j<resultcount-1; j++) {
 				if(result[j]>=result[j+1]) {
 					int temp = result[j];
@@ -44,35 +41,26 @@ public class boj2667 {
 				}
 			}
 		}
-		System.out.println(resultcount-1);
-		for(int i=1; i<resultcount; i++) {
+		System.out.println(resultcount-1);  //단지수 출력
+		for(int i=1; i<resultcount; i++) {  //오름차순으로 출력
 			System.out.println(result[i]);
 		}
 	}
 	
 	public static void DFS(int x, int y) {
+		matrix[x][y] = resultcount;
+		visited[x][y] = true;
+		count++;
 		if((x-1>0 && x-1<=N) && visited[x-1][y] == false && matrix[x-1][y] == 1) {
-			count++;
-			visited[x-1][y] = true;
-			matrix[x-1][y] = resultcount;
 			DFS(x-1, y);
 		}
 		if((y+1>0 && y+1<=N) && visited[x][y+1] == false && matrix[x][y+1] == 1) {
-			count++;
-			visited[x][y+1] = true;
-			matrix[x][y+1] = resultcount;
 			DFS(x, y+1);
 		}
 		if((x+1>0 && x+1<=N) && visited[x+1][y] == false && matrix[x+1][y] == 1) {
-			count++;
-			visited[x+1][y] = true;
-			matrix[x+1][y] = resultcount;
 			DFS(x+1, y);
 		}
 		if((y-1>0 && y-1<=N) && visited[x][y-1] == false && matrix[x][y-1] == 1) {
-			count++;
-			visited[x][y-1] = true;
-			matrix[x][y-1] = resultcount;
 			DFS(x, y-1);
 		}
 	}
