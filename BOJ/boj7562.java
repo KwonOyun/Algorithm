27739bf1c864edf0fdcd2a84c8f51a9c29a31ee7
Oyun <oyoon33@gmail.com>
2018-06-14 21:59:39 +0900

    Add files via upload

--- BOJ/boj7562.java
+++ BOJ/boj7562.java
@@ -24,8 +24,8 @@ public class boj7562 {
 			startY = sc.nextInt();
 			endX = sc.nextInt();
 			endY = sc.nextInt();
-			
 			BFS();
+			
 		}
 	}
 	
@@ -34,12 +34,12 @@ public class boj7562 {
 		queue.add(startX); queue.add(startY);
 		int count=0;
 		while(!queue.isEmpty()) {
-			int size = queue.size();
+			int size = queue.size()/2;
 			while(0<size--) {
 				int presentX = queue.poll();
 				int presentY = queue.poll();
 				if(presentX == endX && presentY == endY) {
-					System.out.print(count);
+					System.out.println(count);
 					break;
 				}
 				visited[presentX][presentY] = true;
@@ -47,11 +47,14 @@ public class boj7562 {
 				int check=0;
 				int[] nextcase = {+1, +2, +2, +1, +2, -1, +1, -2, -1, -2, -2, -1, -2, +1, -1, +2};
 				for(int i=0; i<nextcase.length; i++) {
-					if(i%2==0) nextX = presentX+nextcase[i];
-					else nextY = presentY+nextcase[i];
+					
+					if(i%2==0) nextX = presentX+nextcase[i];  //x좌표 더하기
+					else nextY = presentY+nextcase[i];  //y좌표 더하기
+					
 					check++;
-					if(check%2==1) {
-						if(nextX<0 && nextY<0 && nextX>=I && nextY>=I) continue;
+					if(check%2==0) {
+						if(nextX<0 || nextY<0) continue;
+						if(nextX>=I || nextY>=I) continue;
 						if(visited[nextX][nextY] == true) continue;
 						queue.add(nextX); queue.add(nextY);
 						visited[nextX][nextY] = true;
