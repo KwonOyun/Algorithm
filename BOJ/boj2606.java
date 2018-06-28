5728f78cd0b68f8a5ef51b9fecbc3e2f083458a9
Oyun <oyoon33@gmail.com>
2018-06-29 02:15:40 +0900

    Add files via upload

--- BOJ/boj2606.java
+++ BOJ/boj2606.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
 
 public class boj2606 {
 
+	public static int N, M;
+	public static boolean[][] matrix;
+	public static boolean[] visit;
+	public static int count;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		matrix = new boolean[N+1][N+1];
+		visit = new boolean[N+1];
 		
+		for(int i=0; i<M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			matrix[a][b] = matrix[b][a] = true;
+		}
+		DFS(1);
+		System.out.println(count);
+		
+	}
+	
+	public static Object DFS(int v) {
+		visit[v] = true;
+		for(int i=1; i<=N; i++) {
+			if(matrix[v][i] == true && visit[i] == false) {
+				count++;
+				DFS(i);
+			}
+		}
+		return null;
 	}
 
 }
