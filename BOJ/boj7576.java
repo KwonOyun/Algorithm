85ede1c892834a88caadc4c7acc9d5e68b0d96a5
Oyun <oyoon33@gmail.com>
2018-07-12 22:08:54 +0900

    Add files via upload

--- BOJ/boj7576.java
+++ BOJ/boj7576.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -6,7 +7,7 @@ public class boj7576 {
 
 	public static int M, N;
 	public static int[][] matrix;
-	public static Queue<Integer> queue;
+	public static Queue<Point> queue;
 	public static int count;
 	public static int tomato;
 
@@ -16,45 +17,49 @@ public class boj7576 {
 		Scanner sc = new Scanner(System.in);
 		M = sc.nextInt();
 		N = sc.nextInt();
-		queue = new LinkedList<Integer>();
+		queue = new LinkedList<Point>();
 		matrix = new int[N+1][M+1];
 
 		for(int i=1; i<=N; i++) {
 			for(int j=1; j<=M; j++) {
-				matrix[i][j] = sc.nextInt();
-				if(matrix[i][j] == 1 || matrix[i][j] == -1) tomato++;
-			}
-		}
-
-		for(int i=1; i<=N; i++) { //처음에 1이 였던 거 큐에 추가
-			for(int j=1; j<=M; j++) {
-				if(matrix[i][j] == 1) {
-					queue.add(i); queue.add(j);   //x, y좌표 큐에 추가
+				matrix[i][j] = sc.nextInt();  //토마토 유무 입력받기
+				if(matrix[i][j]==0) continue;
+				
+				else if(matrix[i][j] == 1) {
+					queue.add(new Point(i,j));   //x, y좌표 큐에 추가
+					tomato++;
 				}
+				
+				else if(matrix[i][j] == -1) tomato++;
 			}
 		}
+		BFS();
+		if(tomato !=N*M) System.out.println(-1);
+	}
 
+	public static void BFS() {
 		while(!queue.isEmpty()) {
 			if(tomato == N*M) {
 				System.out.println(count);
 				break;
 			}
-			int size = queue.size()/2;
+			int size = queue.size();
 			while(size-->0) {
-				int a = queue.poll();
-				int b = queue.poll();
+				Point temp = queue.poll();
+				int a = temp.x;
+				int b = temp.y;
 				if(a+1<=N) {
 					if(matrix[a+1][b]==0) {
 						matrix[a+1][b]=1;
 						tomato++;
-						queue.add(a+1); queue.add(b);
+						queue.add(new Point(a+1,b));
 					}
 				}
-				if(a-1>0) {
+				if(a-1>=1) {
 					if(matrix[a-1][b]==0) {
 						matrix[a-1][b]=1;
 						tomato++;
-						queue.add(a-1); queue.add(b);
+						queue.add(new Point(a-1,b));
 					}
 
 				}
@@ -62,30 +67,19 @@ public class boj7576 {
 					if(matrix[a][b+1]==0) {
 						matrix[a][b+1]=1;
 						tomato++;
-						queue.add(a); queue.add(b+1);
+						queue.add(new Point(a,b+1));
 					}
 				}
-				if(b-1>0) {
+				if(b-1>=1) {
 					if(matrix[a][b-1]==0) {
 						matrix[a][b-1]=1;
 						tomato++;
-						queue.add(a); queue.add(b-1);
+						queue.add(new Point(a,b-1));
 					}
 				}
 			}
 			count++;
 		}
-		if(tomato !=N*M) System.out.println(-1);
-
-		//		for(int i=1; i<=N; i++) {  //입력배열 출력
-		//			for(int j=1; j<=M; j++) {
-		//				System.out.print(matrix[i][j]);
-		//			}
-		//			System.out.println();
-		//		}
-
-
-
 	}
 
 }
