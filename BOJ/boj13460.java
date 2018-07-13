9574b88a64f6d74cf5ae14181cf669e419f2a3b6
Oyun <oyoon33@gmail.com>
2018-07-14 01:50:03 +0900

    Add files via upload

--- BOJ/boj13460.java
+++ BOJ/boj13460.java
@@ -7,7 +7,9 @@ public class boj13460 {
 
 	public static int N, M;
 	public static char[][] matrix;
-	public static Queue<Point> queue;
+	public static Queue<Point> Rqueue;
+	public static Queue<Point> Bqueue;
+	public static int count, targetX, targetY;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,26 +17,48 @@ public class boj13460 {
 		N = sc.nextInt();
 		M = sc.nextInt();
 		
-		int targetX, targetY;
 		matrix = new char[N+1][M+1];
-		queue = new LinkedList<Point>();
+		Rqueue = new LinkedList<Point>();
+		Bqueue = new LinkedList<Point>();
 		
 		for(int i=1; i<=N; i++) {    //보드 입력
 			String temp = sc.next();
 			for(int j=1; j<=M; j++) {
 				matrix[i][j] = temp.charAt(j-1);
-				if(matrix[i][j] == 'R' || matrix[i][j] == 'B') {
-					queue.add(new Point(i,j));
-				}
-				else if(matrix[i][j] == 'O') {
-					targetX = i; targetY = j;
-				}
+				if(matrix[i][j] == 'R') Rqueue.add(new Point(i,j));
+				else if(matrix[i][j] == 'B') Bqueue.add(new Point(i,j));
+				else if(matrix[i][j] == 'O') targetX = i; targetY = j;
 			}
 		}
+		
+		Solution();
 	}
 	
 	public static void Solution() {
-		
+		while(!Rqueue.isEmpty()) {
+			int size = Rqueue.size();
+			while(size-->0) {
+				Point rp = Rqueue.poll();
+				Point bp = Bqueue.poll();
+				int Rx = rp.x; int Ry = rp.y;  //Red의 좌표
+				int Bx = bp.x; int By = bp.y;  //Blue의 좌표
+				
+				if(Rx==targetX && Ry<targetY) {
+					count++;
+					System.out.println(count);
+					return;
+				}
+				else {
+					for(int i=2; i<Ry; i++) {
+						if(matrix[i][Ry] == '.') {
+							Rqueue.add(new Point(i, Ry));
+						}
+					}
+				}
+				
+			}
+			count++;
+		}
 	}
 
 }
